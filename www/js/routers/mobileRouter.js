c41dc2c7640ff4eb0e510c1d473f579653260cdf
Alejandro Maggi <alejandro.maggi@hotmail.com>
2014-03-25 10:46:01 -0300

    Env

--- www/js/routers/mobileRouter.js
+++ www/js/routers/mobileRouter.js
@@ -3,7 +3,7 @@ define(["jquery", "backbone", "socketio", "views/joystick"],
 
         var AppRouter = Backbone.Router.extend( {
             initialize: function() {
-                window.socket = io.connect('http://192.168.0.2:3000');
+                window.socket = io.connect('http://192.168.1.233:9000');
                 //window.socket.emit('jugadorConectado', {});
                 //new AppView();
                 this.joystick = new JoystickView();
